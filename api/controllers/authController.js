b2884b0e8a201f65f585eed48ba52bbdf9aca021
ahammedalip <ahammed.puthuppalli@gmail.com>
2023-12-02 16:10:07 +0530

    implemented backend for updating profile

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -30,7 +30,7 @@ export const signin = async (req, res, next) => {
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
         const { password: hashedPass, ...rest } = validUser._doc
         const expiry = new Date(Date.now() + 3600000)
-        res.cookie('acces_token', token, { httpOnly: true, expires: expiry }).status(200).json(rest)
+        res.cookie('access_token', token, { httpOnly: true, expires: expiry }).status(200).json(rest)
     }
     catch (error) {
         next(error)
--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,5 +1,40 @@
+import User from '../models/userModel.js'
+import { errorhandler } from "../utils/errorHandler.js"
+import bcryptjs from 'bcryptjs'
+
+
 export const test = (req, res)=>{
     res.json({
         message: 'API is working'
     })
+}
+
+export const updateUser = async (req,res,next) => {
+    if(req.user.id !== req.params.id ){
+        return next(errorhandler(401, 'You can update only your account!'))
+    }
+
+    try {
+        
+        if(req.body.password){
+            req.body.password = bcryptjs.hashSync(req.body.password, 2)
+        }
+        const updatedUser= await User.findByIdAndUpdate(
+            req.params.id,{
+                $set:{
+                    username:  req.body.username,
+                    email : req.body.email,
+                    password : req.body.password,
+                    profilePicture : req.body.profilePicture
+                }
+            }, {
+                new: true
+            }
+        )
+        const {password, ...rest} = updatedUser._doc;
+        res.status(200).json(rest)
+    } catch (error) {
+       
+        next(error)
+    }
 }
